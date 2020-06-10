c38435e80087f28c99445f879346a627ab07ca7e
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 15:57:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,16 +8,16 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
  
-       {:name=>"Don Gately", :occupation=>"Live-in Staff"}
+      result = [ {:name=>"Don Gately", :occupation=>"Live-in Staff"}
        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
        {:name=>"Pat Monteseian", :occupation=>"Staff"}
        {:name=>"Kate Gompert", :occupation=>"None"}
-       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
        
        binding.pry
        puts "hello"
        
-       result = [NAME, OCCUPATION]
+      
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
