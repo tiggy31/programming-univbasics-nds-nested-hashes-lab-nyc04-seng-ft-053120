8ab863316651d87f84e588c13655cab2cb27e992
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 15:54:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,7 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
  
-    {  NAME>"Don Gately", :occupation=>"Live-in Staff"}
+       {NAME "Don Gately", :occupation=>"Live-in Staff"}
        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
        {:name=>"Pat Monteseian", :occupation=>"Staff"}
        {:name=>"Kate Gompert", :occupation=>"None"}
