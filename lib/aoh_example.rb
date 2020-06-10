669a6a909f2977ed4bb3c71e676c02c0d30044cb
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 16:27:20 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,7 +35,7 @@ def literal_aoh
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"},
        ]
       #binding.pry
- result.sort_by { |name, occupation| age }
+ new= result.sort_by { |name, occupation| name }
 end
 
 def aoh_lookup(aoh, row, key)
