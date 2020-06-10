120fb3d2994dfc6905dcb14ff2809f87132375d3
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 15:32:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,9 +8,9 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
  
-    {:name=>"Don Gately", :occupation=>"Live-in Staff"},
-       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+    {:name=>"Don Gately", :occupation=>"Live-in Staff"}
+       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
+       {:name=>"Pat Monteseian", :occupation=>"Staff"}
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
        
